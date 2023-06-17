c589334025b6b327eb61eb83c2318b956c82a82b
shweta gupta <shweta.gupta249@gmail.com>
2023-06-17 13:26:10 +0530

    update sql and add city country and state data sql

--- DatabaseSeeder/update_database.sql
+++ DatabaseSeeder/update_database.sql
@@ -1,3 +1,49 @@
 /*
     Full database seeder
+    ==========================
+
+CREATE TABLE `astro_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `gender` enum('1','2','0','','') NOT NULL,
+  `mobile_no` varchar(50) NOT NULL,
+  `mail_id` varchar(100) NOT NULL,
+  `mother_name` varchar(100) NOT NULL,
+  `father_name` varchar(100) NOT NULL,
+  `language` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `pincode` varchar(50) NOT NULL,
+  `nationality` varchar(100) NOT NULL,
+  `birth_date` date NOT NULL,
+  `birth_time` varchar(50) NOT NULL,
+  `birth_place` int(11) NOT NULL,
+  `mobile_verify_status` enum('0','1','','') NOT NULL,
+  `mail_verify_status` enum('0','1','','') NOT NULL,
+  `social_login_status` enum('0','1','','') NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique no` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
+CREATE TABLE `astro_countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shortname` varchar(3) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `phonecode` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=249 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+CREATE TABLE `astro_states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `country_id` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4122 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
 */
